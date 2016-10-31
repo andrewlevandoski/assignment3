58b949916ace87ee6cd1ed2a4fad167861d243a4
Andrew Levandoski <agl25@pitt.edu>
2016-10-31 12:19:02 -0400

    adds parent class

--- ChildClass.php
+++ ChildClass.php
@@ -1,6 +1,19 @@
-<?php 
+<?php
 	// this file will extend PArentClass.php
 
 	class ChildClass extends ParentClass {
-		
+		var $make;
+		var $model;
+		var $year;
+		var $color;
+		var $horsepower;
+
+		public function __construct($make, $model, $color, $horsepower) {
+			$this->make       = $make;
+			$this->model      = $model;
+			$this->year       = $year;
+			$this->color      = $color;
+			$this->horsepower = $horsepower;
+		}
+
 	}
--- ParentClass.php
+++ ParentClass.php
@@ -2,5 +2,41 @@
 	// This is the file for the parent class
 
 	class ParentClass {
-		
+		var $make;
+		var $model;
+		var $year;
+		var $color;
+		var $horsepower;
+
+		public function __construct($make, $model, $color, $horsepower) {
+			$this->make       = $make;
+			$this->model      = $model;
+			$this->year       = $year;
+			$this->color      = $color;
+			$this->horsepower = $horsepower;
+		}
+
+		public function getMake() {
+			return $this->make;
+		}
+
+		public function getModel() {
+			return $this->model;
+		}
+
+		public function getYear() {
+			return $this->year;
+		}
+
+		public function getColor() {
+			return $this->color;
+		}
+
+		public function getHorsepower() {
+			return $this->horsepower;
+		}
+
+		public function __toString() {
+			return $this->getColor() . " " . $this->getYear() . " " . $this.getMake() . " " . $this->getModel() . " with " . $this->horsepower . " horsepower.";
+		}
 	}
